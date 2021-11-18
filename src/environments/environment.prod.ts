51bad343ef1a81fe1e05a7ffba3ded7445b55a05
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2021-11-18 02:35:57 -0500

    Colocar firebase credentials en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCU0f2r9lvajADYi_xT5Mx_27o_vDzioGI",
+    authDomain: "noise-monitor-7122c.firebaseapp.com",
+    projectId: "noise-monitor-7122c",
+    storageBucket: "noise-monitor-7122c.appspot.com",
+    messagingSenderId: "111709569263",
+    appId: "1:111709569263:web:3c8ff618e9a57b396f9b1a"
+  },
   baseUri: ''
 };
